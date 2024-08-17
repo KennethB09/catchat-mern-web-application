9826dc2b95963c83cae9e83d18a17b63e1a36b79
Kenneth Bacaltos <kennethbacaltos091@gmail.com>
2024-08-18 03:44:50 +0800

    fix croppedArea

--- client/src/components/UploadImage.tsx
+++ client/src/components/UploadImage.tsx
@@ -55,6 +55,7 @@ export default function UploadImage({ uploadPurpose, userIdOrConversationId }: U
   };
 
   const onCropComplete = useCallback((croppedArea: any, croppedAreaPixels: CroppedAreaPixels) => {
+    console.log(croppedArea);
     setCroppedAreaPixels(croppedAreaPixels);
   }, []);
 
